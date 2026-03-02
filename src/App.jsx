854a21f39a968fb8fe9b62cbbda13865ad6f2270
Paul Davies <paulmdavies@gmail.com>
2026-03-02 18:11:21 +0000

    Handle end of game

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import {Player} from "./components/Player.jsx";
 import {GameBoard} from "./components/GameBoard.jsx";
 import {useState} from "react";
 import {Log} from "./components/Log.jsx";
+import {GameOver} from "./components/GameOver.jsx";
+
+
+const WINNING_COMBINATIONS = [
+    new Set(['00', '01', '02']),
+    new Set(['10', '11', '12']),
+    new Set(['20', '21', '22']),
+    new Set(['00', '10', '20']),
+    new Set(['01', '11', '21']),
+    new Set(['02', '12', '22']),
+    new Set(['00', '11', '22']),
+    new Set(['02', '11', '20'])
+]
 
 
 function getActivePlayerSymbol(turns) {
@@ -17,6 +30,22 @@ function getActivePlayerSymbol(turns) {
 function App() {
   const [turns, setTurns] = useState([])
 
+    let winner = undefined
+    for (const playerSymbol of ['X', 'O']) {
+        const playerTurnCoordinates = new Set(turns.filter(turn => turn.playerSymbol === playerSymbol).map(turn => `${turn.row}${turn.column}`))
+
+        for (const winningCombination of WINNING_COMBINATIONS) {
+            if (winningCombination.isSubsetOf(playerTurnCoordinates)) {
+                console.log(`Player ${playerSymbol} wins`)
+                winner = playerSymbol
+            }
+        }
+
+        if (!winner && turns.length === 9) {
+            winner = null
+        }
+    }
+
   let activePlayerSymbol = getActivePlayerSymbol(turns);
 
   function onSelectSquare(row, column) {
@@ -28,7 +57,7 @@ function App() {
         'row': row,
         'column': column
       }
-      return [turn, ...turns]
+      return [turn, ...currentTurns]
     })
   }
 
@@ -38,6 +67,7 @@ function App() {
         <Player active={activePlayerSymbol === 'X'} name='Player 1' symbol='X'/>
         <Player active={activePlayerSymbol === 'O'} name='Player 2' symbol='O'/>
       </ol>
+        {winner !== undefined && <GameOver winner={winner} />}
       <GameBoard
           onSelectSquare={onSelectSquare}
           turns={turns}
