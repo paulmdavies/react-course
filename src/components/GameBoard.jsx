0254324d65dbc6a744a73f1f77e4694873abcf9b
Paul Davies <paulmdavies@gmail.com>
2026-03-02 12:35:09 +0000

    Make game board clickable

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,26 @@
+import {useState} from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+]
+
 export function GameBoard() {
-    const initialGameBoard = [
-        ['X', null, null],
-        [null, 'O', null],
-        [null, null, null],
-    ]
+    const [gameBoard, updateGameBoard] = useState(initialGameBoard)
+
+    function click(rowIndex, columnIndex) {
+        updateGameBoard((previousGameBoard) => {
+            let newGameBoard = [...previousGameBoard.map(row => [...row])]
+            newGameBoard[rowIndex][columnIndex] = 'X'
+            return newGameBoard
+        })
+    }
 
     return <ol id='game-board'>
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
-                {row.map((playerSymbol, columnIndex) => <button key={columnIndex}>{playerSymbol}</button>)}
+                {row.map((playerSymbol, columnIndex) => <button onClick={() => click(rowIndex, columnIndex)} key={columnIndex}>{playerSymbol}</button>)}
             </ol>
         </li>)}
     </ol>
